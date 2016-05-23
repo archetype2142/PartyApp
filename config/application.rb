998b5f5ff9981597ced79c217de5673c6c07f496
Ritwick <malhotraritwick2011@gmail.com>
2016-05-23 14:04:43 +0530

    as

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.serve_static_assets = true
 config.assets.initialize_on_precompile = false
 
 module Party
