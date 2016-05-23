a07c21a9f2698718d3b7895aafc8017a16e7f55b
Ritwick <malhotraritwick2011@gmail.com>
2016-05-23 16:15:19 +0530

    as

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,15 +15,3 @@
 //= require turbolinks
 //= require_tree .
 
-$(document).ready(function(){
-	// Cache the Window object
-	$window = $(window);
-		$('section[data-type="background"]').each(function(){
-    		var $bgobj = $(this); // assigning the object
-     		$(window).scroll(function() {							
-				var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-				var coords = '50% '+ yPos + 'px';
-				$bgobj.css({ backgroundPosition: coords });
-			});
- 		});	
-	}); 
