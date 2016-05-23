004276dba55ec3f23960e9fbdec6783c1f70b46b
Ritwick <malhotraritwick2011@gmail.com>
2016-05-23 13:29:47 +0530

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,29 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+$(document).ready(function(){
+	// Cache the Window object
+	$window = $(window);
+		$('section[data-type="background"]').each(function(){
+    		var $bgobj = $(this); // assigning the object
+     		$(window).scroll(function() {							
+				var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+				var coords = '50% '+ yPos + 'px';
+				$bgobj.css({ backgroundPosition: coords });
+			});
+ 		});	
+	}); 
