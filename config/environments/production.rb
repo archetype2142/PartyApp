b8ca9c3b4361e9f4e9d7a36ef861d3f117795f09
Ritwick <malhotraritwick2011@gmail.com>
2016-05-24 13:18:52 +0530

    As

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,6 @@ Rails.application.configure do
     :domain  => 'yourdomain.com'
   }
   config.serve_static_assets = true
-config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-config.assets.compile = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
 end
