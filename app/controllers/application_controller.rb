2ebc6ea50ae522579996bbc78b41763e736d2b21
Ritwick <malhotraritwick2011@gmail.com>
2016-06-08 02:04:14 +0530

    admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,8 +19,4 @@ class ApplicationController < ActionController::Base
       }
     end
   end
-
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to main_app.root_path, :alert => exception.message
-  end
 end
