51722e3ef6ad3fd7bbe2af0b74600b9db29220c4
Ritwick <malhotraritwick2011@gmail.com>
2016-06-15 15:18:56 +0530

    unauthorized redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
 
+  class ApplicationController < ActionController::Base
+    rescue_from CanCan::AccessDenied do |exception|
+      redirect_to main_app.root_path, :alert => exception.message
+    end
+  end
 
   protected
 
